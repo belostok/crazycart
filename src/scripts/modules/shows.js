091f1c8ed93bd720e3e5d7118edcaf7ac4cc07c6
belostok <belostok.fl@gmail.com>
2024-03-01 12:46:17 +0300

    events mobile

--- src/scripts/modules/shows.js
+++ src/scripts/modules/shows.js
@@ -1,21 +1,22 @@
 export default () => {
-	const containers = document.querySelectorAll('.js-cc-shows-items');
-	const hideItemsCl = 'cc-shows__items_hide-items';
-	const showButtonCl = 'cc-shows__items_show-button';
+	const container = document.querySelectorAll('.js-cc-shows');
+	const hideItemsCl = 'cc-shows_hide-items';
+	const showButtonCl = 'cc-shows_show-button';
 
-	if ( ! containers.length ) {
+	if ( ! container.length ) {
 		return null;
 	}
 
-	containers.forEach((container) => {
+	container.forEach((container) => {
 		const items = container.querySelectorAll('.js-cc-shows-item');
-		const button = container.querySelector('.js-cc-shows-button');
+		const button = container.parentNode.querySelector('.js-cc-shows-button');
 
 		if ( items.length < 13 || ! button ) {
 			return null;
 		}
 
 		container.classList.add( showButtonCl );
+		container.classList.remove( hideItemsCl );
 		container.classList.remove( hideItemsCl );
 
 		button.addEventListener('click', () => {
